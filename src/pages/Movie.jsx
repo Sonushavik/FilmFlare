f0d57d632455f0ccd11fd7283c4451613bd329da
Sonushavik <sonuk06212@gmail.com>
2025-02-27 21:03:40 +0530

    Adding Movie to H

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,48 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { useLoaderData} from 'react-router'
-import Card from '../components/UI/Card';
-import getMovieData from '../api/GetApiData';
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
+import Card from "../components/UI/Card";
+import getMovieData from "../api/GetApiData";
 
 const Movie = () => {
   const initialMovies = useLoaderData();
   const [moviesData, setMoviesData] = useState(initialMovies);
-  const [movieName,  setMovieName] = useState("")
-// console.log(moviesData);
+  const [movieName, setMovieName] = useState("");
 
-function searchMoviehandler(){
-  setMovieName(event.target.value); 
-  console.log(movieName)
-}
-
-useEffect(() => {
-  if(movieName.length>2){
-    getMovieData(movieName)
-    .then((data)=> {
-      if(data.Search) setMoviesData(data);
-    })
-  }else{
-    setMoviesData(initialMovies)
+  function searchMoviehandler(event) {
+    setMovieName(event.target.value);
   }
-},[movieName,initialMovies]);
+
+  useEffect(() => {
+    if (movieName.length > 2) {
+      getMovieData(movieName)
+        .then((data) => {
+          if (data.Search) {
+            setMoviesData(data);
+          } else {
+            setMoviesData({ Search: [] }); 
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching searched movies:", error);
+          setMoviesData({ Search: [] });
+        });
+    } else {
+      setMoviesData(initialMovies);
+    }
+  }, [movieName, initialMovies]);
 
   return (
     <>
-    <div className="searchBox"
-    style={{display: "flex", justifyContent: "center"}}
-    >
-        <input type="text" id='searchMovie' placeholder='search your favorite Movies ...'
-        value={movieName}
-        onChange={searchMoviehandler}
-        style={{width:"60%", margin: "2rem", borderRadius: "10px"}}
+      <div
+        className="searchBox"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <input
+          type="text"
+          id="searchMovie"
+          placeholder="Search your favorite movies..."
+          value={movieName}
+          onChange={searchMoviehandler}
+          style={{ width: "60%", margin: "2rem", borderRadius: "10px" }}
         />
-    </div>
-    <ul className=" grid grid-four--cols movie-grid">
-    {moviesData.Search.map((curMovie) => {
-      return <Card key={curMovie.imdbID} curMovie = {curMovie}/>;
-    })}
-    </ul>
+      </div>
+      
+      {moviesData?.Search?.length > 0 ? (
+        <ul className="grid grid-four--cols movie-grid">
+          {moviesData.Search.map((curMovie) => (
+            <Card key={curMovie.imdbID} curMovie={curMovie} />
+          ))}
+        </ul>
+      ) : (
+        <p>No movies found</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
